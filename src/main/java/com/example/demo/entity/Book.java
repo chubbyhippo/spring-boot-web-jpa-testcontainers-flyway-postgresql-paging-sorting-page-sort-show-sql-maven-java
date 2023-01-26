f19ac354adfca44f09d224a3464b90efb6a75138
ch <mkmsacc@outlook.com>
2023-01-26 16:01:35 +0700

    Add Book

---
+++ src/main/java/com/example/demo/entity/Book.java
@@ -0,0 +1,27 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String isbn;
+    private String title;
+    private String author;
+    private Double price;
+    private String publisher;
+
+
+}
