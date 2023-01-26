641e30aa40f86fae70deac8049ee8551c21122ea
ch <mkmsacc@outlook.com>
2023-01-26 16:02:12 +0700

    Add BookService

---
+++ src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Book;
+import com.example.demo.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BookService {
+
+    private final BookRepository bookRepository;
+
+    public Page<Book> getBooks(int page, int size) {
+        return bookRepository.findAll(PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "title")));
+    }
+
+}
