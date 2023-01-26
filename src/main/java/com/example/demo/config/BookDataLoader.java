c0a31a92e8cfa19aba6f0acdf84a584e9ba7b1ea
ch <mkmsacc@outlook.com>
2023-01-26 17:12:10 +0700

    Update data loader

--- src/main/java/com/example/demo/config/BookDataLoader.java
+++ src/main/java/com/example/demo/config/BookDataLoader.java
@@ -5,28 +5,28 @@ import com.example.demo.repository.BookRepository;
 import com.github.javafaker.Faker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.stream.Stream;
 
 @Component
 @RequiredArgsConstructor
-public class BookDataLoader implements CommandLineRunner {
+public class BookDataLoader {
 
     private final BookRepository bookRepository;
 
-    @Override
-    public void run(String... args) {
-        var faker = new Faker();
-        Stream.of(1000L).forEach(id ->
-                bookRepository.save(new Book(id,
-                        faker.number().digits(13),
-                        faker.name().title(),
-                        faker.name().fullName(),
-                        faker.number().randomDouble(2, 9, 999),
-                        faker.name().name()))
-        );
-
-
+    @Bean
+    public CommandLineRunner loadBookData() {
+        return args -> {
+            var faker = new Faker();
+            Stream.of(1000L).map(id -> new Book(id,
+                    faker.number().digits(13),
+                    faker.name().title(),
+                    faker.name().fullName(),
+                    faker.number().randomDouble(2, 9, 999),
+                    faker.name().name())).forEach(bookRepository::save);
+        };
     }
+
 }
