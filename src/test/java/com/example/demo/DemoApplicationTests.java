65134fcbb19ba55f943fc7bbaf489586c54c0dbc
ch <mkmsacc@outlook.com>
2023-01-26 17:12:20 +0700

    Update intg test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
+    @Autowired
+    private WebTestClient webTestClient;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void shouldGetPageOfBook() {
+
+
+        webTestClient
+                .get()
+                .uri(uriBuilder -> uriBuilder.path("/api/books")
+                        .queryParam("page", 1)
+                        .queryParam("size", 2)
+                        .build())
+                .exchange()
+                .expectBody()
+                .jsonPath("$.totalElements").isEqualTo(1);
+    }
+
 
 }
