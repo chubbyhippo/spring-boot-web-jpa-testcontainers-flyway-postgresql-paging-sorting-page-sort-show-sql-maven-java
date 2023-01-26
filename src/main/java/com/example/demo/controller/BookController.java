998fb66c70be1a93df3df67142adc7edc92d68d4
ch <mkmsacc@outlook.com>
2023-01-26 16:01:42 +0700

    Add BookController

---
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Book;
+import com.example.demo.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping("/books")
+    public Page<Book> getBooks(@RequestParam int page, @RequestParam int size) {
+        return bookService.getBooks(page, size);
+    }
+}
