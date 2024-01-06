e0b91d8ffdac3e4e834e3466d68dd4eaf550894a
ch <mkmsacc@outlook.com>
2024-01-06 09:55:27 +0700

    Refactor BookRepository interface
    Revised the BookRepository interface to include methods that support paging functionality and altered the extension from JpaRepository to a simple interface. This supports efficient retrieval of large data via pagination, with specific page request parameters.

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
-public interface BookRepository extends JpaRepository<Book, Long> {
+public interface BookRepository{
+    Page<Book> findAll(PageRequest pageRequest);
+    void save(Book book);
 }
